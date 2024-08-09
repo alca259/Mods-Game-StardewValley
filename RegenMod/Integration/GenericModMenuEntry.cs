2b7d564ed866f406e0cab73af0ad74fbc5248f50
Alca259 <alca259@gmail.com>
2024-08-09 23:36:56 +0200

    Setup integration with menu mod config on regeneration

---
+++ RegenMod/Integration/GenericModMenuEntry.cs
@@ -0,0 +1,125 @@
+﻿using Alca259.Common;
+
+namespace RegenMod;
+
+public partial class ModEntry
+{
+    private void SetupGenericModMenu()
+    {
+        // get Generic Mod Config Menu's API (if it's installed)
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (configMenu is null)
+            return;
+
+        configMenu.Register(
+            mod: ModManifest,
+            reset: () =>
+            {
+                _config = new Framework.ModConfig();
+            },
+            save: () => Helper.WriteConfig(_config)
+        );
+
+        configMenu.AddSectionTitle(
+            mod: ModManifest,
+            text: () => "Stamina Regeneration"
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Constantly regenerate stamina",
+            tooltip: () => "Whether to constantly regenerate stamina.",
+            getValue: () => _config.RegenStaminaConstant,
+            setValue: value => _config.RegenStaminaConstant = value
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Stamina regen amount per second",
+            tooltip: () => "The amount of stamina to constantly regenerate per second.",
+            getValue: () => _config.RegenStaminaConstantAmountPerSecond,
+            setValue: value => _config.RegenStaminaConstantAmountPerSecond = value,
+            min: 0.1f,
+            max: 10f
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Regenerate stamina while standing still",
+            tooltip: () => "Whether to regenerate stamina while standing still.",
+            getValue: () => _config.RegenStaminaStill,
+            setValue: value => _config.RegenStaminaStill = value
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Stamina regen amount per second while standing still",
+            tooltip: () => "The amount of stamina to regenerate per second while standing still.",
+            getValue: () => _config.RegenStaminaStillAmountPerSecond,
+            setValue: value => _config.RegenStaminaStillAmountPerSecond = value,
+            min: 0.1f,
+            max: 10f
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Time required to stand still to regenerate stamina",
+            tooltip: () => "The amount of time the player must stand still to regenerate stamina.",
+            getValue: () => _config.RegenStaminaStillTimeRequiredMS,
+            setValue: value => _config.RegenStaminaStillTimeRequiredMS = value,
+            min: 0,
+            max: 5000
+        );
+
+        configMenu.AddSectionTitle(
+            mod: ModManifest,
+            text: () => "Health Regeneration"
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Constantly regenerate health",
+            tooltip: () => "Whether to constantly regenerate health.",
+            getValue: () => _config.RegenHealthConstant,
+            setValue: value => _config.RegenHealthConstant = value
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Health regen amount per second",
+            tooltip: () => "The amount of health to constantly regenerate per second.",
+            getValue: () => _config.RegenHealthConstantAmountPerSecond,
+            setValue: value => _config.RegenHealthConstantAmountPerSecond = value,
+            min: 0.1f,
+            max: 10f
+        );
+
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Regenerate health while standing still",
+            tooltip: () => "Whether to regenerate health while standing still.",
+            getValue: () => _config.RegenHealthStill,
+            setValue: value => _config.RegenHealthStill = value
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Health regen amount per second while standing still",
+            tooltip: () => "The amount of health to regenerate per second while standing still.",
+            getValue: () => _config.RegenHealthStillAmountPerSecond,
+            setValue: value => _config.RegenHealthStillAmountPerSecond = value,
+            min: 0.1f,
+            max: 10f
+        );
+
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Time required to stand still to regenerate health",
+            tooltip: () => "The amount of time the player must stand still to regenerate health.",
+            getValue: () => _config.RegenHealthStillTimeRequiredMS,
+            setValue: value => _config.RegenHealthStillTimeRequiredMS = value,
+            min: 0,
+            max: 5000
+        );
+    }
+}
--- RegenMod/ModEntry.cs
+++ RegenMod/ModEntry.cs
@@ -9,53 +9,47 @@ using SFarmer = StardewValley.Farmer;
 namespace RegenMod;
 
 /// <summary>The main entry point.</summary>
-public class ModEntry : Mod
+public partial class ModEntry : Mod
 {
-    /*********
-    ** Properties
-    *********/
+    #region Fields
     /// <summary>The mod configuration.</summary>
-    private ModConfig Config;
-
+    private ModConfig _config;
     /// <summary>The health regen carried over from the previous tick.</summary>
-    private readonly PerScreen<float> Health = new();
-
+    private readonly PerScreen<float> _health = new();
     /// <summary>The stamina regen carried over from the previous tick.</summary>
-    private readonly PerScreen<float> Stamina = new();
-
+    private readonly PerScreen<float> _stamina = new();
     /// <summary>The time in milliseconds since the player last moved or used a tool.</summary>
-    private readonly PerScreen<double> TimeSinceLastMoved = new();
+    private readonly PerScreen<double> _timeSinceLastMoved = new();
+    #endregion
 
+    #region Properties
     private float ElapsedSeconds => (float)(Game1.currentGameTime.ElapsedGameTime.TotalMilliseconds / 1000);
+    #endregion
 
-
-    /*********
-    ** Public methods
-    *********/
+    #region Override entry point
     /// <summary>The mod entry point, called after the mod is first loaded.</summary>
     /// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
     public override void Entry(IModHelper helper)
     {
         CommonHelper.RemoveObsoleteFiles(this, "RegenMod.pdb");
 
-        Config = helper.ReadConfig<ModConfig>();
+        _config = helper.ReadConfig<ModConfig>();
 
         helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
         helper.Events.Input.ButtonsChanged += OnButtonChanged;
+        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
     }
+    #endregion
 
-
-    /*********
-    ** Private methods
-    *********/
+    #region Event handlers
     /// <inheritdoc cref="IInputEvents.ButtonsChanged"/>
     /// <param name="sender">The event sender.</param>
     /// <param name="e">The event arguments.</param>
     private void OnButtonChanged(object sender, ButtonsChangedEventArgs e)
     {
-        if (Config.ReloadKey.JustPressed())
+        if (_config.ReloadKey.JustPressed())
         {
-            Config = Helper.ReadConfig<ModConfig>();
+            _config = Helper.ReadConfig<ModConfig>();
             Monitor.Log("Config reloaded", LogLevel.Info);
         }
     }
@@ -71,58 +65,87 @@ public class ModEntry : Mod
         SFarmer player = Game1.player;
 
         //detect movement or tool use
-        TimeSinceLastMoved.Value += Game1.currentGameTime.ElapsedGameTime.TotalMilliseconds;
+        _timeSinceLastMoved.Value += Game1.currentGameTime.ElapsedGameTime.TotalMilliseconds;
         if (player.timerSinceLastMovement == 0)
-            TimeSinceLastMoved.Value = 0;
+        {
+            _timeSinceLastMoved.Value = 0;
+        }
+
         if (player.UsingTool)
-            TimeSinceLastMoved.Value = 0;
+        {
+            _timeSinceLastMoved.Value = 0;
+        }
 
         // health regen
-        if (Config.RegenHealthConstant)
-            Health.Value += Config.RegenHealthConstantAmountPerSecond * ElapsedSeconds;
-        if (Config.RegenHealthStill)
-        {
-            if (TimeSinceLastMoved.Value > Config.RegenHealthStillTimeRequiredMS)
-                Health.Value += Config.RegenHealthStillAmountPerSecond * ElapsedSeconds;
-        }
-        if (player.health + Health.Value >= player.maxHealth)
-        {
-            player.health = player.maxHealth;
-            Health.Value = 0;
-        }
-        else if (Health.Value >= 1)
-        {
-            player.health += 1;
-            Health.Value -= 1;
-        }
-        else if (Health.Value <= -1)
-        {
-            player.health -= 1;
-            Health.Value += 1;
-        }
+        HealthRegenTick(player);
 
         // stamina regen
-        if (Config.RegenStaminaConstant)
-            Stamina.Value += Config.RegenStaminaConstantAmountPerSecond * ElapsedSeconds;
-        if (Config.RegenStaminaStill)
+        StaminaRegenTick(player);
+    }
+
+    private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+    {
+        SetupGenericModMenu();
+    }
+    #endregion
+
+    #region Methods
+    private void StaminaRegenTick(SFarmer player)
+    {
+        if (_config.RegenStaminaConstant)
         {
-            if (TimeSinceLastMoved.Value > Config.RegenStaminaStillTimeRequiredMS)
-                Stamina.Value += Config.RegenStaminaStillAmountPerSecond * ElapsedSeconds;
+            _stamina.Value += _config.RegenStaminaConstantAmountPerSecond * ElapsedSeconds;
         }
-        if (player.Stamina + Stamina.Value >= player.MaxStamina)
+
+        if (_config.RegenStaminaStill && _timeSinceLastMoved.Value > _config.RegenStaminaStillTimeRequiredMS)
+        {
+            _stamina.Value += _config.RegenStaminaStillAmountPerSecond * ElapsedSeconds;
+        }
+
+        if (player.Stamina + _stamina.Value >= player.MaxStamina)
         {
             player.Stamina = player.MaxStamina;
-            Stamina.Value = 0;
+            _stamina.Value = 0;
         }
-        else if (Stamina.Value >= 1)
+        else if (_stamina.Value >= 1)
         {
             player.Stamina += 1;
-            Stamina.Value -= 1;
+            _stamina.Value -= 1;
         }
-        else if (Stamina.Value <= -1)
+        else if (_stamina.Value <= -1)
         {
             player.Stamina -= 1;
-            Stamina.Value += 1;
+            _stamina.Value += 1;
         }
     }
+
+    private void HealthRegenTick(SFarmer player)
+    {
+        if (_config.RegenHealthConstant)
+        {
+            _health.Value += _config.RegenHealthConstantAmountPerSecond * ElapsedSeconds;
+        }
+
+        if (_config.RegenHealthStill && _timeSinceLastMoved.Value > _config.RegenHealthStillTimeRequiredMS)
+        {
+            _health.Value += _config.RegenHealthStillAmountPerSecond * ElapsedSeconds;
+        }
+
+        if (player.health + _health.Value >= player.maxHealth)
+        {
+            player.health = player.maxHealth;
+            _health.Value = 0;
+        }
+        else if (_health.Value >= 1)
+        {
+            player.health += 1;
+            _health.Value -= 1;
+        }
+        else if (_health.Value <= -1)
+        {
+            player.health -= 1;
+            _health.Value += 1;
+        }
+    }
+    #endregion
 }
