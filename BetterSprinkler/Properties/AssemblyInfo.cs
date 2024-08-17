7b31d3609e6abcd0132b32b0c17ce83e062a5f88
Alca259 <alca259@gmail.com>
2024-08-17 14:05:15 +0200

    Add mod Better sprinklers from https://gitlab.com/speeder1/SMAPISprinklerMod/-/tree/master?ref_type=heads for customization.

---
+++ BetterSprinkler/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("BetterSprinklers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyFileVersion("2.4.0")]
